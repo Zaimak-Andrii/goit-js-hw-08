005832f5bdc78ecec8cf1dcb0830b492abe67a08
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2022-11-09 17:50:27 +0200

    Done 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('div.gallery');
+
+let galleryMarkup = galleryItems
+  .map(item => {
+    return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+
+      />
+    </a>
+  </div>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = galleryMarkup;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
